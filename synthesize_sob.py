77a6f16d4b07e965ce60926fae8a6f5de4bfa812
QiaoyuYang <qiaoyu.kelvin.yang@outlook.com>
2024-11-11 23:08:41 -0500

    Add support to synthesize audio from midi track

---
+++ synthesize_sob.py
@@ -0,0 +1,52 @@
+import fluidsynth
+import numpy
+import pyaudio
+import wave
+import argparse
+
+CHANNELS = 2
+RATE = 44100
+FORMAT = pyaudio.paInt16
+out_len_in_s = 30
+
+def synthesize(instrument, part):
+    with wave.open(f'output_{part}.wav', 'wb') as wf:
+        
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(pyaudio.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+
+        fs = fluidsynth.Synth()
+        fs.custom_router_callback = None
+
+        sfid_trombone = fs.sfload(f"soundfonts/{instrument}.sf2")
+        fs.program_select(0, sfid_trombone, 0, 0)
+
+        fs.play_midi_file(f"bc_no2_mv1/bc_no2_mv1_{part}.mid")
+
+        # Generate seconds of audio into s
+        s = []
+        for _ in range(out_len_in_s):
+            s = numpy.append(s, fs.get_samples(RATE))
+            if fluidsynth.fluid_player_get_status(fs.player) != fluidsynth.FLUID_PLAYER_PLAYING:
+                break
+        fs.delete()
+            
+        samps = fluidsynth.raw_audio_string(s)
+        
+        wf.writeframes(samps)
+
+def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--instrument", type=str, default="trombone", help="The instrument of the track")
+    parser.add_argument("-p", "--part", type=str, default="trombone", help="The part name of the track")
+    args = parser.parse_args()
+    instrument = args.instrument
+    part = args.part
+    
+    synthesize(instrument, part)
+
+
+if __name__ == "__main__":
+    main()
